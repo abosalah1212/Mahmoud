840f0b629127f049445708a82de858a5f6b6084f
mahmoud <mhmoudsalah1586@yahoo.com>
2018-10-31 22:42:19 +0200

    comment

--- app/src/main/java/com/example/mahmoud/mahmoud/MainActivity.java
+++ app/src/main/java/com/example/mahmoud/mahmoud/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"text",Toast.LENGTH_LONG).show();
 
 
 
